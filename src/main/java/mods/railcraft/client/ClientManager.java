fb996a3c4ca0bcc43691f23dbc77bba0f384a56b
Robert Harries <robertpharries@gmail.com>
2024-07-04 21:42:19 +1000

    xd

--- src/main/java/mods/railcraft/client/ClientManager.java
+++ src/main/java/mods/railcraft/client/ClientManager.java
@@ -266,7 +266,7 @@ public class ClientManager {
     boolean isSnapshot = qualifier != null && qualifier.equals("snapshot");
     boolean showMessageBeta = Railcraft.BETA && RailcraftConfig.CLIENT.showMessageBeta.get();
     if (!FMLLoader.isProduction() || isSnapshot || showMessageBeta) {
-      var type = isSnapshot ? "development" : "beta";
+      var type = isSnapshot ? "mreow" : "beta";
       var issueUrl = ((ModFileInfo) (modInfo.getOwningFile())).getIssueURL().toString();
       var message = CommonComponents.joinLines(
           Component.literal("You are using a %s version of %s.".formatted(type, RailcraftConstants.NAME))
